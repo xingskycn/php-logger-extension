56a60821c51d5c8dcf414973e3ad5321ff3d9d84
jarismar <jarismar@71378f66-b35b-11dd-99fa-ddd771690bd5>
2009-07-28 03:08:08 +0000

    Implemented LoggerManager and Logger classes. It is now possible to do basic log to file.

--- trunk/logger.cpp
+++ trunk/logger.cpp
@@ -31,6 +31,7 @@
 #include <log4cxx/logger.h>
 #include <log4cxx/basicconfigurator.h>
 #include <log4cxx/propertyconfigurator.h>
+#include <log4cxx/logmanager.h>
 
 using namespace log4cxx;
 
@@ -38,11 +39,22 @@ ZEND_DECLARE_MODULE_GLOBALS(logger)
 static PHP_GINIT_FUNCTION(logger);
 
 /* NTS globals var */
-zend_class_entry *basic_configurator_ce;
-zend_object_handlers basic_configurator_object_handlers;
+static zend_class_entry *logger_basic_configurator_ce;
+static zend_object_handlers logger_basic_configurator_object_handlers;
 
-zend_class_entry *property_configurator_ce;
-zend_object_handlers property_configurator_object_handlers;
+static zend_class_entry *logger_property_configurator_ce;
+static zend_object_handlers logger_property_configurator_object_handlers;
+
+static zend_class_entry *logger_manager_ce;
+static zend_object_handlers logger_manager_object_handlers;
+
+static zend_class_entry *logger_ce;
+static zend_object_handlers logger_object_handlers;
+struct logger_object {
+	zend_object std;
+	LoggerPtr logger;
+};
+
 
 static int le_logger;
 
@@ -65,11 +77,11 @@ zend_module_entry logger_module_entry = {
 	logger_functions,
 	PHP_MINIT(logger),
 	PHP_MSHUTDOWN(logger),
-	PHP_RINIT(logger),		/* Replace with NULL if there's nothing to do at request start */
-	PHP_RSHUTDOWN(logger),	/* Replace with NULL if there's nothing to do at request end */
+	PHP_RINIT(logger),
+	PHP_RSHUTDOWN(logger),
 	PHP_MINFO(logger),
 #if ZEND_MODULE_API_NO >= 20010901
-	PHP_LOGGER_VERSION, /* Replace with version number for your extension */
+	PHP_LOGGER_VERSION,
 #endif
 	PHP_MODULE_GLOBALS(logger),
 	PHP_GINIT(logger),
@@ -118,6 +130,85 @@ function_entry logger_property_configurator_methods[] = {
 	{NULL, NULL, NULL}
 };
 
+/* {{{ proto public static Logger getLogger(string $name)
+   Returns the specific logger. */
+static PHP_METHOD(LoggerManager, getLogger)
+{
+	char *name;
+	int name_len;
+
+	if (FAILURE == zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &name, &name_len)) {
+		RETURN_FALSE;
+	}
+
+	/* create an instance of Logger */
+	object_init_ex(return_value, logger_ce);
+	logger_object *obj = (logger_object *)zend_object_store_get_object(return_value TSRMLS_CC);
+	obj->logger = LogManager::getLogger(name);
+}
+/* }}} */
+
+function_entry logger_manager_methods[] = {
+	PHP_ME(LoggerManager, getLogger, NULL, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
+	{NULL, NULL, NULL}
+};
+
+void logger_free_storage(void *object TSRMLS_DC)
+{
+	logger_object *obj = (logger_object *)object;
+	// delete obj->logger; not needed because obj->logger is statically handled by log4cxx
+	
+	zend_hash_destroy(obj->std.properties);
+	FREE_HASHTABLE(obj->std.properties);
+	
+	efree(obj);
+}
+
+zend_object_value logger_create_handler(zend_class_entry *type TSRMLS_DC)
+{
+	zval *tmp;
+	zend_object_value retval;
+	
+	logger_object *obj = (logger_object *)emalloc(sizeof(logger_object));
+	memset(obj, 0, sizeof(logger_object));
+	obj->std.ce = type;
+	
+	ALLOC_HASHTABLE(obj->std.properties);
+	zend_hash_init(obj->std.properties, 0, NULL, ZVAL_PTR_DTOR, 0);
+	zend_hash_copy(obj->std.properties, &type->default_properties, 
+			(copy_ctor_func_t)zval_add_ref, (void *)&tmp, sizeof(zval *));
+	
+	retval.handle = zend_objects_store_put(obj, NULL,
+			logger_free_storage, NULL TSRMLS_CC);
+	retval.handlers = &logger_object_handlers;
+	
+	return retval;
+}
+
+/* {{{ proto public void getLogger(string $message)
+   Log a message with the DEBUG level. */
+static PHP_METHOD(Logger, debug)
+{
+	char *message;
+	int message_len;
+	zval *object = getThis();
+
+	if (FAILURE == zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &message, &message_len)) {
+		return;
+	}
+
+	logger_object *obj = (logger_object *)zend_object_store_get_object(object TSRMLS_CC);
+	//obj->logger->debug(message);
+	LOG4CXX_DEBUG(obj->logger, message);
+}
+/* }}} */
+
+function_entry logger_methods[] = {
+	PHP_ME(Logger, debug, NULL, ZEND_ACC_PUBLIC)
+	{NULL, NULL, NULL}
+};
+
+
 
 /* {{{ PHP_INI
  */
@@ -142,17 +233,32 @@ PHP_MINIT_FUNCTION(logger)
 
 	/* initialize class LoggerBasicConfigurator */ 
 	INIT_CLASS_ENTRY(ce, "LoggerBasicConfigurator", logger_basic_configurator_methods);
-	basic_configurator_ce = zend_register_internal_class(&ce TSRMLS_CC);
-	memcpy(&basic_configurator_object_handlers,
+	logger_basic_configurator_ce = zend_register_internal_class(&ce TSRMLS_CC);
+	memcpy(&logger_basic_configurator_object_handlers,
 	        zend_get_std_object_handlers(), sizeof(zend_object_handlers));
-	basic_configurator_object_handlers.clone_obj = NULL;
-	
+	logger_basic_configurator_object_handlers.clone_obj = NULL;
+
 	/* initialize class LoggerPropertyConfigurator */
 	INIT_CLASS_ENTRY(ce, "LoggerPropertyConfigurator", logger_property_configurator_methods);
-	property_configurator_ce = zend_register_internal_class(&ce TSRMLS_CC);
-	memcpy(&property_configurator_object_handlers,
+	logger_property_configurator_ce = zend_register_internal_class(&ce TSRMLS_CC);
+	memcpy(&logger_property_configurator_object_handlers,
 	        zend_get_std_object_handlers(), sizeof(zend_object_handlers));
-	property_configurator_object_handlers.clone_obj = NULL;
+	logger_property_configurator_object_handlers.clone_obj = NULL;
+
+	/* initialize class LoggerManager */
+	INIT_CLASS_ENTRY(ce, "LoggerManager", logger_manager_methods);
+	logger_manager_ce = zend_register_internal_class(&ce TSRMLS_CC);
+	memcpy(&logger_manager_object_handlers,
+	        zend_get_std_object_handlers(), sizeof(zend_object_handlers));
+	logger_manager_object_handlers.clone_obj = NULL;
+
+	/* initialize class Logger */
+	INIT_CLASS_ENTRY(ce, "Logger", logger_methods);
+	logger_ce = zend_register_internal_class(&ce TSRMLS_CC);
+	logger_ce->create_object = logger_create_handler;
+	memcpy(&logger_object_handlers,
+	        zend_get_std_object_handlers(), sizeof(zend_object_handlers));
+	logger_object_handlers.clone_obj = NULL;
 
 	return SUCCESS;
 }
